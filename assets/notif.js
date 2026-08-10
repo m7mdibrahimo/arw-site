cdf5a5b79856343d47e4b0b2a263ee28a024481f
m7mdibrahimo <44986524+m7mdibrahimo@users.noreply.github.com>
2026-08-10 22:41:27 +0300

    Update notif.js

--- assets/notif.js
+++ assets/notif.js
@@ -240,7 +240,7 @@
       try {
         new Notification('عرب راسلنج 🔔', {
           body: 'تم تفعيل إشعارات العروض والملخصات المترجمة بنجاح! ستتوصل بجديد العروض والملخصات فور نشرها.',
-          icon: '/favicon.svg',
+          icon: '/favicon.png',
           dir: 'rtl',
           lang: 'ar'
         });
